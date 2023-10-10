4d30db8f75fbabb33b82691b1cd08a3c403057d0
Otávio Ferreira <ottvferreira@gmail.com>
2023-10-10 13:21:35 -0300

    Incluindo Entity Usuario

---
+++ src/main/java/com/bancosimplesproject/entity/UsuarioEntity.java
@@ -0,0 +1,39 @@
+package com.bancosimplesproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "usuario")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String sobrenome;
+
+    @Column(unique = true)
+    private String cpfcnpj;
+
+    @Column(unique = true)
+    private String email;
+
+    private String senha;
+
+    private BigDecimal saldo;
+
+    private Tipo tipoUsuario;
+
+}
