adb42e9aa956daaaae081658c93709b182ca1d32
Otávio Ferreira <ottvferreira@gmail.com>
2023-10-10 13:00:17 -0300

    Inclusão de Spring.io

---
+++ src/main/java/com/bancosimplesproject/BancosimplesprojectApplication.java
@@ -0,0 +1,13 @@
+package com.bancosimplesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancosimplesprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancosimplesprojectApplication.class, args);
+	}
+
+}
