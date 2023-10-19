d236ca57c6b45ecb288e7065f9693a4625f8ede0
Otávio Ferreira <ottvferreira@gmail.com>
2023-10-19 19:19:10 -0300

    Tratando valores e adicionando ao banco de dados

--- src/main/java/com/bancosimplesproject/entity/transacao/TransacaoEntity.java
+++ src/main/java/com/bancosimplesproject/entity/transacao/TransacaoEntity.java
@@ -2,10 +2,7 @@ package com.bancosimplesproject.entity.transacao;
 
 import com.bancosimplesproject.entity.usuario.UsuarioEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -15,6 +12,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class TransacaoEntity {
     @Id
--- src/main/java/com/bancosimplesproject/service/TransacaoService.java
+++ src/main/java/com/bancosimplesproject/service/TransacaoService.java
@@ -1,6 +1,7 @@
 package com.bancosimplesproject.service;
 
 import com.bancosimplesproject.dto.TransacaoDTO;
+import com.bancosimplesproject.entity.transacao.TransacaoEntity;
 import com.bancosimplesproject.entity.usuario.UsuarioEntity;
 import com.bancosimplesproject.repository.TransacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @Service
@@ -35,6 +37,23 @@ public class TransacaoService {
         if (!autorizado){
             throw new Exception("Transação não autorizada");
         }
+
+        TransacaoEntity transacaoEntity = new TransacaoEntity();
+        transacaoEntity.setValor(transacaoDTO.value());
+        transacaoEntity.setUsuarioenvio(usuarioenvioid);
+        transacaoEntity.setUsuariorecebe(usuariorecebeid);
+        transacaoEntity.setTr_data(LocalDateTime.now());
+
+        // Altera saldo do Usuario de envio subtraindo transacao
+        usuarioenvioid.setSaldo(usuarioenvioid.getSaldo().subtract(transacaoDTO.value()));
+
+        // Altera saldo do Usuario que recebe somando transacao
+        usuariorecebeid.setSaldo(usuariorecebeid.getSaldo().add(transacaoDTO.value()));
+
+        this.transacaoRepository.save(transacaoEntity);
+        this.usuarioService.salvarUsuario(usuarioenvioid);
+        this.usuarioService.salvarUsuario(usuariorecebeid);
+
     }
 
     public boolean autorizarTransacao (UsuarioEntity usuarioenvio, BigDecimal valor){
